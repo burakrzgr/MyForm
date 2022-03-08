08c5ca8cde9a8bd47ea4c16fcd222e23b2c5cfca
Burak Rüzgar <62022339+burakrzgr@users.noreply.github.com>
2022-03-08 23:59:01 +0300

    style

--- src/components/new-form/CreateNewForm.tsx
+++ src/components/new-form/CreateNewForm.tsx
@@ -3,29 +3,28 @@ import { Button, Container, Form } from "react-bootstrap";
 import AddComponent from "./AddComponent"
 import AskPersonnelInfo from "./AskPersonnelInfo";
 
-export default function CreateNewForm() {  
-    const [personnelInfo,setPersonnelInfo] = React.useState<string>("0");
-    const [formName,setFormName] = React.useState<string>("");
+export default function CreateNewForm() {
+    const [personnelInfo, setPersonnelInfo] = React.useState<string>("0");
+    const [formName, setFormName] = React.useState<string>("");
     return (
-        <Container >
+        <Container className="pt-5">
             <h2>New Form</h2>
             <hr></hr>
-            <h4>Form Name</h4>
-            <Form>
-                <Form.Group className="mb-3" controlId="formbasicName">
-                  
-                    <Form.Label><h4>Form Name</h4></Form.Label>
+            <Form className="pt-5 text-start">
+                <Form.Group className="pt-5" controlId="formbasicName">
+                    <Form.Label><h5>Form Name</h5></Form.Label>
                     <Form.Control type="text" placeholder="Form Name" />
                 </Form.Group>
-                <Form.Group className="mb-3" controlId="formBasicPrInfo">
+                <Form.Group className="pt-5 " controlId="formBasicPrInfo">
                     <Form.Label>Ask Personnel Information (Name, Surname, Identity Number...)</Form.Label>
-                    <AskPersonnelInfo selected={personnelInfo} selectedChanged={(vl:any) => setPersonnelInfo(vl)} ></AskPersonnelInfo>
+                    <div style={styles.formControl}>
+                        <AskPersonnelInfo selected={personnelInfo} selectedChanged={(vl: any) => setPersonnelInfo(vl)} ></AskPersonnelInfo>
+                    </div>
                     <Form.Text className="text-muted">
-                        Please notice that checking this are will force surveyor to share their personal information. Such as name and surname. 
+                        Please notice that checking this are will force surveyor to share their personal information. Such as name and surname.
                     </Form.Text>
                 </Form.Group>
-
-                <Form.Group className="mb-3" controlId="formBasicPassword">
+                <Form.Group className="pt-5" controlId="formBasicPassword">
                     <Form.Label>Password</Form.Label>
                     <Form.Control type="password" placeholder="Password" />
                 </Form.Group>
@@ -35,8 +34,14 @@ export default function CreateNewForm() {
                 <Button variant="primary" type="submit">
                     Create New Form
                 </Button>
-                </Form>
+            </Form>
             <AddComponent></AddComponent>
         </Container>
     );
 }
+
+const styles = {
+    formControl: {
+        padding: '.375rem .75rem'
+    }
+  } as const;
