5b30ae1c13476a8d0997ed76d8c6eeba319122cd
Burak <burakrzgr@gmail.com>
2022-03-09 09:59:23 +0300

    update

--- src/components/new-form/PopupForAdd.tsx
+++ src/components/new-form/PopupForAdd.tsx
@@ -1,10 +1,19 @@
-import { Button, Modal } from "react-bootstrap"
+import React, { useState,useEffect } from "react";
+import { Button, Form, Modal } from "react-bootstrap";
+import "./form-input.css";
 
 
 export default function PopupForAdd({ show, closeHandle, addedHandle }: { show: boolean, closeHandle: Function, addedHandle: Function }) {
+    const[value,SetValue] = useState<string>("");
+    
     const addCriteria=() =>{
-        addedHandle("Lolllloll");
+        addedHandle(value);
     }
+
+    useEffect(() => {
+      SetValue("");
+    }, [show])
+    
     return (
         <Modal
             show={show}
@@ -17,12 +26,20 @@ export default function PopupForAdd({ show, closeHandle, addedHandle }: { show: 
             <Modal.Header closeButton>
                 <Modal.Title
                     id="contained-modal-title-vcenter"
-                    style={{ color: '#000' }}
                 >
                     Yeni Alan Ekle
                 </Modal.Title>
             </Modal.Header>
-            <Modal.Body style={{ color: '#000' }}>
+            <Modal.Body >
+                <Form.Group className="" controlId="formtextBox">
+                    <Form.Label><h5>Form Name</h5></Form.Label>
+                    <Form.Control 
+                        type="text" 
+                        placeholder="Form Name" 
+                        className="form-underline"
+                        value={value} 
+                        onChange={(vl : any) => SetValue(vl.target.value)} />
+                </Form.Group>
             </Modal.Body>
             <Modal.Footer className="justify-content-between">
                 <Button variant="outline-dark" onClick={() => closeHandle()}>Cancel</Button>
