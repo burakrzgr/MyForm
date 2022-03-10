c40ee54dd354fa3670bad72fa8be41a56c6500df
Burak Rüzgar <62022339+burakrzgr@users.noreply.github.com>
2022-03-10 23:50:29 +0300

    date time

--- src/components/new-form/new-component/DateTime.tsx
+++ src/components/new-form/new-component/DateTime.tsx
@@ -1,19 +1,25 @@
+import { useState } from "react";
 import { Col, Form, FormControl, Row, Stack } from "react-bootstrap";
 
 
 export default function DateTime() {
+    const [date, setDate] = useState<boolean>(true);
+    const [time, setTime] = useState<boolean>(true);
     return (
         <>
-
             <Stack direction="horizontal">
-                <div className="p-2">
-                    <Form.Label>Date</Form.Label>
-                    <FormControl type="date" id="myDate" style={{ width: "10rem" }}></FormControl>
-                </div>
-                <div className="p-2">
-                    <Form.Label>Time</Form.Label>
-                    <FormControl type="time" id="myTime" style={{ width: "10rem" }}></FormControl>
-                </div>
+                {date ?
+                    <div className="p-2">
+                        <Form.Label>Date</Form.Label>
+                        <FormControl type="date" id="myDate" style={{ width: "10rem" }}></FormControl>
+                    </div> : <></>
+                }
+                {time ?
+                    <div className="p-2">
+                        <Form.Label>Time</Form.Label>
+                        <FormControl type="time" id="myTime" style={{ width: "10rem" }}></FormControl>
+                    </div> : <></>
+                }
             </Stack>
         </>
     );
