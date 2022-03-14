50226fe8f270c13d82edc4b5d44bc2d3f79465c3
Burak Rüzgar <62022339+burakrzgr@users.noreply.github.com>
2022-03-14 08:08:19 +0300

    remove from selection

--- src/components/new-form/new-field/Selections.tsx
+++ src/components/new-form/new-field/Selections.tsx
@@ -1,13 +1,19 @@
 import { useState } from "react";
-import { Button, Form, FormControl, InputGroup } from "react-bootstrap";
+import { Button, Form, FormControl, InputGroup, Stack } from "react-bootstrap";
 
-export function SelectionList({options,setOptions, check : multi, setCheck : setMulti}:{options:{ items: string[] },setOptions:Function,  check :boolean,setCheck : Function,}) {
+export function SelectionList({ options, setOptions, check: multi, setCheck: setMulti }: { options: { items: string[] }, setOptions: Function, check: boolean, setCheck: Function, }) {
     const addNewOption = (vl: string) => {
         if (vl !== "" && !options.items.includes(vl)) {
             let list = options;
             list.items.push(vl);
             setOptions({ ...list });
         }
+    };
+    const removeOption = (vl: string) => {
+        let items = {  items: options.items.filter(x => {
+            return(x !== vl)
+        })};
+        setOptions({...items});
     };
     return (
         <>
@@ -20,11 +26,11 @@ export function SelectionList({options,setOptions, check : multi, setCheck : set
                 ></Form.Check>
             </Form.Group>
             <AddOption optionAdded={addNewOption}></AddOption>
-            <DisplayOptions list={options.items} allowMulti={multi}></DisplayOptions>
+            <DisplayOptions list={options.items} allowMulti={multi} ItemRemove={removeOption}></DisplayOptions>
         </>
     );
 }
-export function SelectionCombo({options,setOptions}:{options:{ items: string[] },setOptions:Function}) {
+export function SelectionCombo({ options, setOptions }: { options: { items: string[] }, setOptions: Function}) {
     const addNewOption = (vl: string) => {
         if (vl !== "" && !options.items.includes(vl)) {
             let list = options;
@@ -32,30 +38,44 @@ export function SelectionCombo({options,setOptions}:{options:{ items: string[] }
             setOptions({ ...list });
         }
     };
+    const removeOption = (vl: string) => {
+        let items = {  items: options.items.filter(x => {
+            return(x !== vl)
+        })};
+        setOptions({...items});
+    };
+    const [selected,setSelected] = useState<string>("");
     return (
         <>
             <AddOption optionAdded={addNewOption}></AddOption>
-            <Form.Select aria-label="Select option" className="mt-3 control-shadow">
-                <option>[Please select one of the options...]</option>
-                {options.items.map(i => (
-                    <option value={i} key={i}>{i}</option>
-                ))}
-            </Form.Select>
+            <Stack direction="horizontal">
+                <Form.Select aria-label="Select option" className="mt-3 control-shadow me-auto" onChange={(e) => setSelected(e.target.value)}>
+                    <option value="-">[Please select one of the options...]</option>
+                    {options.items.map(i => (
+                        <option value={i} key={i}>{i}</option>
+                    ))}
+                </Form.Select>
+                <Button variant="outline-danger ms-2 mt-3" onClick={() => removeOption(selected)}>🗑️</Button>
+            </Stack>
         </>
     );
 }
 
-function DisplayOptions({ list, allowMulti }: { list: string[]; allowMulti: boolean; }) {
+function DisplayOptions({ list, allowMulti, ItemRemove }: { list: string[]; allowMulti: boolean; ItemRemove: Function }) {
     return (
         <>
             {list.map((i) => (
                 <Form.Group key={i} className="mt-3">
-                    <Form.Check
-                        type={allowMulti ? "checkbox" : "radio"}
-                        label={i}
-                        checked={false}
-                        onChange={(e: any) => { }}
-                    ></Form.Check>
+                    <Stack direction="horizontal">
+                        <Form.Check
+                            type={allowMulti ? "checkbox" : "radio"}
+                            label={i}
+                            checked={false}
+                            onChange={(e: any) => { }}
+                            className="me-auto"
+                        ></Form.Check>
+                        <Button size="sm" variant="outline-danger" onClick={() => ItemRemove(i)}>🗑️</Button>
+                    </Stack>
                 </Form.Group>
             ))}
         </>
--- src/components/new-form/show-field/FieldDisplay.tsx
+++ src/components/new-form/show-field/FieldDisplay.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { FieldoForm } from "../FieldofForm";
+import { FieldoForm } from "../class/FieldofForm";
 import { TextField,TextArea, FieldSelect, FieldCombo, FieldCheck, FieldDateTime, FieldRate, FieldAcceptPolicy} from "./Field";
 
 export default function FieldDisplay({
@@ -9,20 +9,25 @@ export default function FieldDisplay({
   areas: { items: Array<FieldoForm> };
   setAreas: Function;
 }) {
+
+  const remove = (item :FieldoForm) => {
+  let list = areas.items.filter(x => {return (x !== item)});
+    setAreas({items:list});
+  }
   return (
     <>
       {areas.items.map((i, key) => (
         <React.Fragment key={key}>
           {
             {
-              "tx_b": <TextField item={i} />,
-              "tx_a": <TextArea item={i} />,
-              "sel_": <FieldSelect item={i} />,
-              "cm_b": <FieldCombo item={i} />,
-              "ch_b": <FieldCheck item={i} />,
-              "date": <FieldDateTime item={i} />,
-              "rate": <FieldRate item={i} />,
-              "ap_c": <FieldAcceptPolicy item={i} />,
+              "tx_b": <TextField item={i} removeEvent={(i : FieldoForm) => remove(i)} />,
+              "tx_a": <TextArea item={i} removeEvent={(i : FieldoForm) => remove(i)}/>,
+              "sel_": <FieldSelect item={i} removeEvent={(i : FieldoForm) => remove(i)}/>,
+              "cm_b": <FieldCombo item={i} removeEvent={(i : FieldoForm) => remove(i)}/>,
+              "ch_b": <FieldCheck item={i} removeEvent={(i : FieldoForm) => remove(i)}/>,
+              "date": <FieldDateTime item={i} removeEvent={(i : FieldoForm) => remove(i)}/>,
+              "rate": <FieldRate item={i} removeEvent={(i : FieldoForm) => remove(i)}/>,
+              "ap_c": <FieldAcceptPolicy item={i} removeEvent={(i : FieldoForm) => remove(i)}/>,
             }[i.type]
           }
         </React.Fragment>
