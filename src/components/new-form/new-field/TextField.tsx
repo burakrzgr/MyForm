0d07654fd400aa3cded693ba73e3dede8c3dc7c6
Burak <burakrzgr@gmail.com>
2022-03-14 13:18:53 +0300

    remove unused

--- src/components/new-form/new-field/TextField.tsx
+++ src/components/new-form/new-field/TextField.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import { Button, Form, FormControl, InputGroup, Stack } from "react-bootstrap";
 
 export function TextBox({
