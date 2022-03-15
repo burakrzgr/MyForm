cb5e795d65eb0c53f674b700dae497bc48a706bb
Burak Rüzgar <62022339+burakrzgr@users.noreply.github.com>
2022-03-16 00:12:16 +0300

    axios-setup

--- src/axios/new-form.tsx
+++ src/axios/new-form.tsx
@@ -1,9 +1,9 @@
 import { FieldoForm } from "../components/new-form/class/FieldofForm";
 import { MyForm } from "../components/new-form/class/MyForm";
+import axios from "axios";
 
 
-
-const GetForm =  (id:number) => {};
+const GetForm =  (id:number) => {return axios.get("https://localhost:7213/Form/"+id);}
 const GetFormCriteria =  (criteria:string) => {};
 const postForm =  (id:number,form : MyForm) => {};
 const deleteForm =  (id : number) => {};
