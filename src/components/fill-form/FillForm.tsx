97b0c9abfe1d0f5841922b7a4128c8b17dcdac6b
Burak Rüzgar <62022339+burakrzgr@users.noreply.github.com>
2022-03-23 22:12:36 +0300

    fill Form development

--- src/components/fill-form/FillForm.tsx
+++ src/components/fill-form/FillForm.tsx
@@ -9,39 +9,42 @@ import { FieldoForm } from "../new-form/class/FieldofForm";
 import { GetForm } from "../../axios/new-form";
 
 
-export default function FillForm({}) {
-    const [form,setForm] = useState<MyForm>();
+export default function FillForm({ }) {
+    const [form, setForm] = useState<MyForm>();
     const { id } = useParams<string>();
     useEffect(() => {
+        console.log("dd");
         GetForm(Number(id))
-        .then((val : MyForm) => setForm(val))
-        .catch((e) => console.log("GetFormError", e));
+            .then((val) => setForm(val.data))
+            .catch((e) => console.log("GetFormError", e));
     }, [id])
-    
-    return(
+
+    return (
         <>
-            <Container className="pt-5">
-            <Form className="text-start">
-                <Form.Group className="pt-4" controlId="formbasicName">
-                    <Form.Label><h4>{form?.formName}</h4></Form.Label>
-                </Form.Group>
-                <Form.Group className="pt-4" controlId="formbasicName">
-                    <Form.Label><h6>{form?.formDesc}</h6></Form.Label>
-                </Form.Group>
-                <Form.Group className="pt-4 " controlId="formBasicPrInfo">
-                    <Form.Label>Ask Personnel Information (Name, Surname, Identity Number...)</Form.Label>
-                    <div style={mystyle.formControl}>
-                        <DisplayPersonnelInfo selected={form ? form.personellInfo :""} selectedChanged={() => {}}></DisplayPersonnelInfo>
+            {form ?
+                <Container className="pt-5">
+                    <Form className="text-start">
+                        <Form.Group className="pt-4" controlId="formbasicName">
+                            <Form.Label><h4>{form?.formName}</h4></Form.Label>
+                        </Form.Group>
+                        <Form.Group className="pt-4" controlId="formbasicName">
+                            <Form.Label><h6>{form?.formDesc}</h6></Form.Label>
+                        </Form.Group>
+                        <Form.Group className="pt-4 " controlId="formBasicPrInfo">
+                            <Form.Label>Ask Personnel Information (Name, Surname, Identity Number...)</Form.Label>
+                            <div style={mystyle.formControl}>
+                                <DisplayPersonnelInfo selected={form ? form.personellInfo : ""} selectedChanged={() => { }}></DisplayPersonnelInfo>
+                            </div>
+                            <Form.Text className="text-muted">
+                                Please notice that changing this area might/will force participant to share their personal information. Such as name and surname.
+                            </Form.Text>
+                        </Form.Group>
+                        <FieldDisplay areas={{ items: form ? form.questions : ([] as Array<FieldoForm>) }} setAreas={() => { }} ></FieldDisplay>
+                    </Form>
+                    <div className="pt-5">
+                        <Button variant="primary" size="lg">Submit Form</Button>
                     </div>
-                    <Form.Text className="text-muted">
-                        Please notice that changing this area might/will force participant to share their personal information. Such as name and surname.
-                    </Form.Text>
-                </Form.Group>
-                <FieldDisplay areas={{items : form ? form.questions : ([] as Array<FieldoForm>)}} setAreas={() => {}} ></FieldDisplay>
-            </Form>
-            <div className="pt-5">
-                <Button variant="primary" size="lg">Submit Form</Button>
-            </div>
-        </Container>
+                </Container>
+                : <div>Yükleniyor!!</div>}
         </>);
 }
