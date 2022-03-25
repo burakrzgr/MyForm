bfafa583a35a3948ffc1cb3c5cc2a814272fea5b
Burak Rüzgar <62022339+burakrzgr@users.noreply.github.com>
2022-03-25 22:03:00 +0300

    creating class for component

--- src/components/new-form/class/FieldofForm.tsx
+++ src/components/new-form/class/FieldofForm.tsx
@@ -16,3 +16,63 @@ export type Display ={
     date:boolean,
     time:boolean 
 };
+
+
+export type FormTemplate = {
+    id : number,
+    creatorId : GUID,
+    createDate : Date,
+    questions : Array<QuestionTemplate>,
+
+}
+
+export type QuestionTemplate = {
+    id : number,
+    formTemplate : FormTemplate,
+    questionType : enumQuestionType,
+    questionText : string,
+    answerArea : any,
+}
+
+export type AnswerTemplate_Text ={
+    defaultText : string,
+    textHeight : number 
+}
+
+export type AnswerTemplate_Selection ={
+    templateId? : number,
+    options? : string[],
+    multi : boolean
+}
+export type AnswerTemplate_Check ={
+    checkText : string,
+    defaultChecked? : boolean,
+    description : string
+}
+export type AnswerTemplate_Date ={
+    checkDate : boolean,
+    checkTime : boolean
+}
+
+
+
+
+export type GUID = string & { isGuid: true};
+
+function guid(guid: string) : GUID {
+    return  guid as GUID; // maybe add validation that the parameter is an actual guid ?
+}
+
+export enum enumQuestionType
+{
+    TextBox = 1,
+    TextArea = 2,
+    RadioButton = 3,
+    ComboBox = 4,
+    CheckBox = 5,
+    DateTime = 6,
+    Rate = 7,
+    Upload = 8,
+    Acceptpolicy = 9,
+    Info = 10
+}
