66708dc0a52d6de1ebaf7605de8cead7490fb90f
Burak Rüzgar <62022339+burakrzgr@users.noreply.github.com>
2022-03-27 22:39:10 +0300

    fill - form başlandı

--- src/components/new-form/custom-component/DisplayPersonnelInfo.tsx
+++ src/components/new-form/custom-component/DisplayPersonnelInfo.tsx
@@ -1,4 +1,4 @@
-import { ButtonGroup, ToggleButton } from "react-bootstrap";
+import { Form, Stack } from "react-bootstrap";
 import { enumPersonelInfo } from "../class/FormTemplate";
 
 export default function DisplayPersonnelInfo({selected,selectedChanged }:{selected:enumPersonelInfo,selectedChanged:Function}) {
@@ -7,9 +7,14 @@ export default function DisplayPersonnelInfo({selected,selectedChanged }:{select
         <>
             {
                 {
-                    "0" : <>Dont ask any information</>,
-                    "1" : <>Only if participant accepts.</>,
-                    "32" : <>Make required field</>
+                    "0" : <p className="text-success"><u>You are anonymous!</u> No İnformation will be shared with form creator.</p>,
+                    "1" : 
+                        <Stack direction="horizontal">
+                            <div className="text-warning"><u>You want to inform who you are?</u></div>
+                            <div className="ps-3"><Form.Check value="yes" checked={false} onChange={() => {}} ></Form.Check></div>
+                            <div className="ps-2">Yes</div>
+                        </Stack>,
+                    "2" : <p className="text-danger"><u>Your information will be shared with form creator!</u></p>
                 }[String(selected)]
             }
         </>
