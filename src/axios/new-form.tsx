4e284ab3971abd1fb4363dad259973074316178e
Burak Rüzgar <62022339+burakrzgr@users.noreply.github.com>
2022-03-27 22:40:56 +0300

    axios

--- src/axios/new-form.tsx
+++ src/axios/new-form.tsx
@@ -1,4 +1,4 @@
-import { FieldoForm } from "../components/new-form/class/FormTemplate";
+import { FieldoForm, FormTemplate } from "../components/new-form/class/FormTemplate";
 import { MyForm } from "../components/new-form/class/MyForm";
 import axios from "axios";
 
@@ -6,7 +6,7 @@ import axios from "axios";
 export const GetForm =  (id:number) => {return axios.get("https://localhost:7213/Form/"+id);}
 export const GetAllForm =  () => {return axios.get("https://localhost:7213/Form");}
 export const GetFormCriteria =  (criteria:string) => {};
-export const postForm =  (id:number,form : MyForm) => {};
+export const postForm =  (id:number,form : FormTemplate) => {return axios.post("https://localhost:7213/Form",form);};
 export const deleteForm =  (id : number) => {};
 export const putForm =  (form : MyForm) => {};
 export const patchForm =  (form : any) => {};
