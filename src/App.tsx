c22ae752a1589c62436bb3471ffda6ae29cc8710
Burak Rüzgar <62022339+burakrzgr@users.noreply.github.com>
2022-04-07 21:59:56 +0300

    style

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,3 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import "bootstrap/dist/css/bootstrap.min.css";
 import HeaderNavbar from "./components/HeaderNavbar";
--- src/Splash.tsx
+++ src/Splash.tsx
@@ -1,49 +1,55 @@
 import logo from "./logo.svg";
-import React from "react";
 import { GetToken } from "./database/Token";
 import { Button } from "react-bootstrap";
-import { ReactIcon } from "./FontAwesome";
+import { GithubIcon, ReactIcon } from "./FontAwesome";
 
 export default function Splash() {
-    return(
+    return (
         <>
             <img src={logo} className="App-logo" alt="logo" />
-        <p>
-            This project developed with </p>
-        <code>React TypeScript & Bootstrapt</code>
-        <p> by Burak Rüzgar.</p>
             <p>
-                <ReactIcon mystyle={{paddingRight:"1rem"}}></ReactIcon>
-                <a
-                    className="App-link"
+                This project developed with </p>
+            <code>React TypeScript & Bootstrapt</code>
+            <p> by Burak Rüzgar.</p>
+            <div>
+                <Button
+                    variant="dark"
+                    className="ps-4 pe-4 border-info mb-2"
                     href="https://reactjs.org"
                     target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Learn React
-                </a>
-            </p>
-            <a
-                className="App-link"
-                href="https://github.com/burakrzgr"
-                target="_blank"
-                rel="noopener noreferrer"
-            >
-                About Me
-            </a>
+                    rel="noopener noreferrer" >
+                    <ReactIcon mystyle={{ paddingRight: "1rem" }}></ReactIcon>
+                    <a className="App-link">
+                        Learn React
+                    </a>
+                </Button>
+            </div>
+            <div>
+                <Button
+                    variant="dark"
+                    className="ps-4 pe-4 border-info mb-2"
+                    href="https://github.com/burakrzgr"
+                    target="_blank"
+                    rel="noopener noreferrer" >
+                    <GithubIcon mystyle={{ paddingRight: "1rem" }}></GithubIcon>
+                    <a className="App-link">
+                        About Me
+                    </a>
+                </Button>
+            </div>
             <hr></hr>
-            <div className="p-2">Welcome Back {GetToken()?.userName??""}</div>
+            <div className="p-2">Welcome Back {GetToken()?.userName ?? ""}</div>
             <div className="p-2">
-            <Button variant="outline-primary">Primary outline</Button>
+                <Button variant="outline-primary">Primary outline</Button>
             </div>
             <div className="p-2">
-            <Button variant="primary">Primary Main</Button>
+                <Button variant="primary">Primary Main</Button>
             </div>
             <div className="p-2">
-            <Button variant="outline-system">System outline</Button>
+                <Button variant="outline-system">System outline</Button>
             </div>
             <div className="p-2">
-            <Button variant="system">System Main</Button>
+                <Button variant="system">System Main</Button>
             </div>
         </>
     );
