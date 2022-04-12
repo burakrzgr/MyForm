ede06b8479a7101b1300e5ad006d8919ed385601
Burak <burakrzgr@gmail.com>
2022-04-12 17:46:39 +0300

    role eklendi

--- src/components/auth/UserModal.tsx
+++ src/components/auth/UserModal.tsx
@@ -2,6 +2,13 @@ export type UserModal ={
     userId : number,
     userName : string,
     password : string,
+    role? : Role,
     token : string,
     tokenExpiration : Date
 };
+
+export enum Role{
+    Participant,
+    Admin,
+    Receiver
+}
