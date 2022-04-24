803dfd944f97f4709ba058be6b46fc94a48ed9f2
Burak Rüzgar <62022339+burakrzgr@users.noreply.github.com>
2022-04-24 22:48:19 +0300

    format

--- src/components/auth/LoginPage.tsx
+++ src/components/auth/LoginPage.tsx
@@ -7,11 +7,11 @@ import { UserModal } from "./UserModal";
 import { useNavigate } from "react-router-dom";
 
 export default function LoginPage() {
-    const [username,setUsername] = useState("");
-    const [password,setPassword] = useState("");
-    const [error,setError] = useState("");
-    const [fadingIn,setFadingIn] = useState(false);
-    const displayError = (message:string) => {
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+    const [error, setError] = useState("");
+    const [fadingIn, setFadingIn] = useState(false);
+    const displayError = (message: string) => {
         setError(message);
         setFadingIn(true);
         setTimeout(() => {
@@ -19,16 +19,16 @@ export default function LoginPage() {
         }, 5000);
     }
     let navigate = useNavigate();
-    const setTokenData = (data : UserModal)=>{
+    const setTokenData = (data: UserModal) => {
         SetToken(data);
         navigate("/");
     }
-    const CreateNewAccount =() => {
+    const CreateNewAccount = () => {
         navigate("../Register");
     }
     const login = () => {
-        const result = Login({userName: username,password :password, userId:0,token:"",roles:[],tokenExpiration:new Date() })
-        result.then(x => {x.data.isSuccess?setTokenData(x.data.data):displayError(x.data.message)}).catch(ex => console.log(ex));
+        const result = Login({ userName: username, password: password, userId: 0, token: "", roles: [], tokenExpiration: new Date() })
+        result.then(x => { x.data.isSuccess ? setTokenData(x.data.data) : displayError(x.data.message) }).catch(ex => console.log(ex));
     }
     return (
         <Container >
@@ -37,31 +37,31 @@ export default function LoginPage() {
                     <div className="col-lg-12 login-title">
                         LOGIN
                     </div>
-                        <div className="col-lg-12 login-form">
-                            {<div className={"text-danger pb-1 " + (fadingIn?'fadeIn':'fadeOut')} >{error}</div>}
-                            <Form>
-                                <Form.Group>
-                                    <Form.Label className="form-control-label login">Username</Form.Label>
-                                    <Form.Control className="login" type="text" value={username} onChange={(val) => setUsername(val.target.value)} />
-                                </Form.Group>
-                                <Form.Group>
-                                    <Form.Label className="form-control-label login">Password</Form.Label>
-                                    <Form.Control className="login" type="password" value={password} onChange={(val) => setPassword(val.target.value)} />
-                                </Form.Group>
-                                <div className="col-lg-12 loginbttm">
-                                    <div className="col-lg-6 login-btm login-text">
-                                    </div>
-                                    <div className="col-lg-6 login-btm login-button w-100">
-                                        <Button variant="outline-primary login" className="ps-4 pe-4 login" onClick={login} disabled={!(password.length>2&&username.length>2)}>LOGIN</Button>
-                                    </div>
+                    <div className="col-lg-12 login-form">
+                        {<div className={"text-danger pb-1 " + (fadingIn ? 'fadeIn' : 'fadeOut')} >{error}</div>}
+                        <Form>
+                            <Form.Group>
+                                <Form.Label className="form-control-label login">Username</Form.Label>
+                                <Form.Control className="login" type="text" value={username} onChange={(val) => setUsername(val.target.value)} />
+                            </Form.Group>
+                            <Form.Group>
+                                <Form.Label className="form-control-label login">Password</Form.Label>
+                                <Form.Control className="login" type="password" value={password} onChange={(val) => setPassword(val.target.value)} />
+                            </Form.Group>
+                            <div className="col-lg-12 loginbttm">
+                                <div className="col-lg-6 login-btm login-text">
                                 </div>
-                            </Form>
+                                <div className="col-lg-6 login-btm login-button w-100">
+                                    <Button variant="outline-primary login" className="ps-4 pe-4 login" onClick={login} disabled={!(password.length > 2 && username.length > 2)}>LOGIN</Button>
+                                </div>
+                            </div>
+                        </Form>
 
-                        </div>
+                    </div>
                     <div className="col-lg-12 ">
-                                    <div style={{marginTop: "115px", marginBottom: "20px"}} className="border-bottom border-light"/>
-                                    <Form.Label>Dont have account <a className="link-info" onClick={CreateNewAccount}>create a new one.</a></Form.Label>
-                                </div>
+                        <div style={{ marginTop: "115px", marginBottom: "20px" }} className="border-bottom border-light" />
+                        <Form.Label>Dont have account <a className="link-info" onClick={CreateNewAccount}>create a new one.</a></Form.Label>
+                    </div>
                 </div>
             </div>
         </Container>
