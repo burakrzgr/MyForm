c70c3b618cb6fb72422fc11e84cf84a52d6dbf7c
Burak Rüzgar <62022339+burakrzgr@users.noreply.github.com>
2022-04-29 22:37:54 +0300

    style

--- src/components/completed/ViewQuestion.tsx
+++ src/components/completed/ViewQuestion.tsx
@@ -3,7 +3,7 @@ import { CompletedQuestion } from "./class/CompletedForm";
 export default function ViewQuestion({question}: {question:CompletedQuestion}) {
     return(
         <div className="pt-3 pe-2">
-            <h5>{question.questionText}</h5>
+            <h5><u>{question.questionText}</u></h5>
             <p>{question.answer}</p>
         </div>
     );
