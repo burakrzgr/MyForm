2c15b66ec9da50bfbf8a68d9bac24dc82658b7a6
Burak Rüzgar <62022339+burakrzgr@users.noreply.github.com>
2022-06-16 21:18:51 +0300

    redirect  - test

---
+++ src/components/completed/history/Redirect.tsx
@@ -0,0 +1,18 @@
+import { Button, Form, FormControl, InputGroup } from "react-bootstrap";
+
+
+export default function History({ formId }: { formId: number }) {
+    return (
+        <>
+            <InputGroup>
+                <Form.Select aria-label="Select option" className="mt-3 control-shadow me-auto">
+                    <option value="-">[Select the user you want to see...]</option>
+                        <option value="1" >Person 1</option>
+                        <option value="2" >Person 2</option>
+                        <option value="3" >Person 3</option>
+                </Form.Select>
+                <Button variant="outline-danger ms-2 mt-3" onClick={() => {}}>Send!</Button>
+            </InputGroup>
+        </>
+    );
+}
